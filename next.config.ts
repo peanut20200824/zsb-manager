773598a0f71cf36c12157460a3755ec535ec5c1d
peanut02 <shourui1988@gmail.com>
2026-01-17 16:38:44 +0800

    feat: 添加静态文件，准备部署到Gitee Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
